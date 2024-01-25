acebdb9f4b7d4b96885d611e61632489b6f1cfca
redmoon02 <parkcy.luna.02@gmail.com>
2024-01-25 22:39:24 +0900

    create hamburger-menu

---
+++ hamburger/hamburger.js
@@ -0,0 +1,20 @@
+document.querySelector('.hamburger-menu').addEventListener('click', () => {
+    document.querySelector('.sidebar').style.display = 'flex';
+});
+
+document.querySelector('.sidebar').addEventListener('click', () => {
+    document.querySelector('.sidebar').style.display = 'none';
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
